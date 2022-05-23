9ac958358f75d84cad1f12ceece188ca808c7551
zeonga1102 <dlwjddk4587@naver.com>
2022-05-23 17:41:58 +0900

    post 수정 기능 추가

---
+++ static/JS/detail_js.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$('#floating_edit_button').hide();
+});
+
+function edit_button_click() {
+  $('#edit_desc').css('display', 'block');
+  $('#desc').hide();
+  $('#floating_modal_button').hide();
+  $('#option_modal').modal("hide")
+  $('#floating_edit_button').show();
+}
+
+function done_button_click(post_id) {
+  let desc = $('#edit_desc').val();
+
+  $.ajax({
+    type: 'POST',
+    url: '/detail/edit',
+    data: {post_id: post_id, desc: desc},
+    success: function (response) {
+      window.location.reload();
+    }
+  });
+}
