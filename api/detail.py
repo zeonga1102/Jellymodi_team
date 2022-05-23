9825c29807b6618d29bba3cb4b527d43ebb5e182
zeonga1102 <dlwjddk4587@naver.com>
2022-05-23 13:39:26 +0900

    SSR 방식으로 detail page에 post info 전달

--- api/detail.py
+++ api/detail.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, Blueprint
+from bson.objectid import ObjectId
+
+from pymongo import MongoClient
 
 bp = Blueprint('detail', __name__, url_prefix='/detail')
 
-@bp.route('/')
-def show_detail():
-    print('here')
-    return render_template('detail.html')
+@bp.route('/<post_id>')
+def show_detail(post_id):
+    post_info = db.posts.find_one({'_id': ObjectId(post_id)})
+    post_info['_id'] = str(post_info['_id'])
+    return render_template('detail.html', post_info)
